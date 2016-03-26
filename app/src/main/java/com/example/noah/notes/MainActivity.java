8e00237f40cf1c1155eaf3cdf7c5a75b34d42f54
Noah Ingham <noahingham@gmail.com>
2016-03-26 19:39:47 +1100

    Working on NotesView

--- app/src/main/java/com/example/noah/notes/MainActivity.java
+++ app/src/main/java/com/example/noah/notes/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.noah.notes;
 
+import android.support.v4.app.ListFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,10 +15,18 @@ public class MainActivity extends AppCompatActivity {
 
     List l = new ArrayList<Note>();
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new NotesView())
+                    .commit();
+        }
+
+
     }
 
 
