cfd50f09f169017396bd5fec28d7b9ad0a5e3042
Noah Ingham <noahingham@gmail.com>
2016-03-28 23:57:23 +1100

    Fixed bugs

--- app/src/androidTest/java/com/example/noah/notes/ApplicationTest.java
+++ app/src/androidTest/java/com/example/noah/notes/ApplicationTest.java
@@ -2,11 +2,14 @@ package com.example.noah.notes;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
+import android.test.suitebuilder.annotation.LargeTest;
 import android.test.suitebuilder.annotation.MediumTest;
+import android.widget.EditText;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
+
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
@@ -17,6 +20,11 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
     public void testNoteCreate() {
         createApplication();
 
-        // Do stuff
+        Note newNote = new LocalNote(getContext(), "testNote");
+
+        newNote.write(getContext(), "testing, 1234");
+
+        ((MainActivity) getContext()).loadNote(newNote.getName());
+
     }
 }
--- app/src/androidTest/java/com/example/noah/notes/MarkupTest.java
+++ app/src/androidTest/java/com/example/noah/notes/MarkupTest.java
@@ -16,6 +16,24 @@ public class MarkupTest extends ApplicationTestCase<Application> {
     @Test
     public void testBold() {
         String testString = "**This is bold**, **this isn't.";
+
+        // Render
+        String newString = MarkupRenderer.render(testString);
+        String expectedString = "<strong>This is bold</strong>, **this isn't.";
+        assertEquals(newString, expectedString);
+
+        // Edit
+        String newString2 = MarkupRenderer.editor(testString);
+        String expectedString2 = "<strong>*This is bold*</strong>, **this isn't.";
+        assertEquals(newString2, expectedString2);
+
+        // Preview
+        /***/
+    }
+
+    @Test
+    public void testItalic() {
+        String testString = "**This is bold**, **this isn't.";
         String newString = MarkupRenderer.render(testString);
 
         // Render
